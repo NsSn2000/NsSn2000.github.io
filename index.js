0f77675f1fffcb0591874b27e0dc6f41d541c16b
NsSn2000 <123253313+NsSn2000@users.noreply.github.com>
2023-01-21 20:48:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,12 +6,14 @@ btn.addEventListener('click', () => {
 
     if (value == "No" || value == "Fuck No") {
         document.getElementById("menu").innerHTML = ` You chose ${value} :(( fr??, me iz sad.`;
-        document.getElementById("gif").style.visibility = 'none';
-        document.getElementById("gif").width = '0';
-        document.getElementById("gif").height = '0';
+        
         document.getElementById("gif2").width = '1100';
         document.getElementById("gif2").height = '900';
         document.getElementById("gif2").style.visibility = 'visible';
+        document.getElementById("gif").width = '0';
+        document.getElementById("gif").height = '0';
+        document.getElementById("gif").style.visibility = 'none';
+
 
 
     } else {
@@ -19,12 +21,14 @@ btn.addEventListener('click', () => {
 
         document.getElementById("menu").innerHTML = `
             Cherrie wants you to know that she loves you more <33333`;
-         document.getElementById("gif2").style.visibility = 'none';
-        document.getElementById("gif2").width = '0';
-        document.getElementById("gif2").height = '0';
+        
         document.getElementById("gif").width = '1100';
         document.getElementById("gif").height = '900';
         document.getElementById("gif").style.visibility = 'visible';
+        document.getElementById("gif2").width = '0';
+        document.getElementById("gif2").height = '0';
+        document.getElementById("gif2").style.visibility = 'none';
+
 
 
     }
